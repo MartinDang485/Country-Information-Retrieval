893f12b6bd726f70017183bb454d174afe8b4a15
Martin <martdang5@gmail.com>
2023-10-03 21:46:29 -0400

    Worked on the js some more, added textContent from api mostly

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-let country = document.querySelector('#searchbar');
+const country = document.querySelector('#searchbar');
+const submit = document.querySelector('#search-button');
 
+let countryName = document.querySelector('#country-name');
+let continent = document.querySelector('#continent');
+let moreInfoHeader = document.querySelector('#more-information');
+let currency = document.querySelector('#currency');
+let population = document.querySelector('#population');
+let flag = document.querySelector('#flag');
 
-let submit = document.querySelector('#search-button');
-submit.addEventListener('click' ,() => {
-    yes(country);
+submit.addEventListener('click',() => {
+    yes(country.value);
 })
 
 function yes(country) {
-    console.log(country.value);
+    console.log(country);
+
+ fetch('https://restcountries.com/v3.1/name/' + country)
+ .then(response => {
+    return response.json()
+ })
+ .then(data => {
+    console.log(data)
+
+    countryName.textContent = country;
+    continent.textContent = 'Continent: ' + data[0].continents;
+    moreInfoHeader.textContent = 'More About ' + country;
+    currency.textContent = 'Currency: ' + data[0].currencies.CAD.name;
+    population.textContent = 'Population: ' + data[0].population;
+ })
 }
